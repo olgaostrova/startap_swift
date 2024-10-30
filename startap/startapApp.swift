25d1777b3e6a02c09d38072a44a05d3a37c11f4f
Olga Ostroverkh <ostrova22@mail.ru>
2024-10-30 11:34:56 +0300

    init commit

---
+++ startap/startapApp.swift
@@ -0,0 +1,17 @@
+//
+//  startapApp.swift
+//  startap
+//
+//  Created by Ksenia Svidunovich on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct startapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
